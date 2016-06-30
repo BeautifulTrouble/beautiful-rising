8dd2f8103682376fd1808dc3d2f4479da22b962d
Adrian Carpenter <adriatic.c@gmail.com>
2016-06-30 13:36:28 -0700

    Add <div height-polling="100ms" (heightchanged)="action($event)"> directive

--- app/utilities.ts
+++ app/utilities.ts
@@ -1,7 +1,7 @@
 // General purpose stuff goes in here.
 
 import { Http, Response } from '@angular/http';
-import { Pipe, PipeTransform, Component, Input, OnInit } from '@angular/core';
+import { Pipe, PipeTransform, Component, Input, OnInit, ElementRef, Directive, Output, EventEmitter, NgZone } from '@angular/core';
 import { DomSanitizationService } from '@angular/platform-browser/src/security/dom_sanitization_service';
 
 import { Observable } from 'rxjs/Observable';
@@ -71,5 +71,55 @@ export class SVGComponent implements OnInit {
 }
 class SVGCache {
     static cache = {};
+}
+
+
+@Directive({
+    /* Directive which sends (heightchanged) and (widthchanged) events for the given element
+     *
+     * Example uses w/ & w/o polling interval in milliseconds:
+     *  <div height-polling (heightchanged)="action($event)">
+     *  <div height-polling="100" (heightchanged)="action($event)">
+     *  <div width-polling="100ms" (widthchanged)="action($event)">
+     *  <div height-polling width-polling="100" (heightchanged)="action($event)" (widthchanged)="action()">
+     */
+    selector: '[height-polling], [width-polling]'
+})
+export class SizePollingDirective {
+    @Output() heightchanged = new EventEmitter();
+    @Output() widthchanged = new EventEmitter();
+    defaultInterval = 100;
+
+    constructor(private zone: NgZone, private el: ElementRef) {
+        var hInterval = el.nativeElement.getAttribute('height-polling');
+        var wInterval = el.nativeElement.getAttribute('width-polling');
+        hInterval = hInterval ? parseInt(hInterval) : hInterval === '' ? this.defaultInterval : null;
+        wInterval = wInterval ? parseInt(wInterval) : wInterval === '' ? this.defaultInterval : null;
+
+        // Angular has wrapped every call to setInterval with change detection logic using
+        // zone.js, so we run this polling outside of Angular's zone for better performance
+        this.zone.runOutsideAngular(() => {
+            if (hInterval) this.hIntervalId = setInterval(() => {
+                var hNew = el.nativeElement.clientHeight;
+                if (hNew != this.hLast) {
+                    this.heightchanged.emit(hNew);
+                    this.hLast = hNew;
+                    this.zone.run();    // Force change detection
+                }
+            }, hInterval);
+            if (wInterval) this.wIntervalId = setInterval(() => {
+                var wNew = el.nativeElement.clientWidth;
+                if (wNew != this.wLast) {
+                    this.widthchanged.emit(wNew);
+                    this.wLast = wNew;
+                    this.zone.run();    // Force change detection
+                }
+            }, wInterval);
+        });
+    }
+    ngOnDestroy() {
+        this.hIntervalId && clearInterval(this.hIntervalId);
+        this.wIntervalId && clearInterval(this.wIntervalId);
+    }
 }
 
