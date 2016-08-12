55f25549197503aa0f91666498d5b1aaf79bedd4
Adrian Carpenter <adriatic.c@gmail.com>
2016-08-11 22:51:46 -0700

    New build script ensures parallel builds do not interfere

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Not nearly enough sanity checking
+if [ -z "$BRANCH" ]; then
+    echo "This script is unsafe to run manually."
+    exit 1
+fi
+
+
+BRANCH=$BRANCH
+BUILD=build.$BRANCH
+
+# This isn't critical but ensures git-pull in the cloned repo happens quickly
+git checkout $BRANCH
+git pull
+
+# Create a clone of the repo and ensure it's _still_ on the right branch
+mkdir $BUILD && cd $BUILD
+git clone .. .
+git checkout $BRANCH
+git pull
+ln -s ../node_modules
+
+# Build the site
+npm run webpack
+ln -s ../assets dist/assets
+
+# Return to the top-level directory and move the new site into place
+cd ..
+rm -rf $BRANCH
+mv $BUILD/dist $BRANCH
+rm -rf $BUILD
+
